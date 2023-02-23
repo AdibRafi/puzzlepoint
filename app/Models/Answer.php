2fde5a0c578675f97330b2f76df86f7e3032813b
Abdjb <63862086+Abdjb@users.noreply.github.com>
2023-02-23 12:39:03 +0800

    added phpDocBlock

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Answer
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Answer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Answer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Answer query()
+ * @mixin \Eloquent
+ */
 class Answer extends Model
 {
     use HasFactory;
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,6 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Question
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Question newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Question newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Question query()
+ * @mixin \Eloquent
+ */
 class Question extends Model
 {
     use HasFactory;
