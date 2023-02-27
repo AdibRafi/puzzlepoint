761d9036eba17d41d41c4e91241e5ff5cdfe943c
Abdjb <63862086+Abdjb@users.noreply.github.com>
2023-02-27 14:37:39 +0800

    add fontawesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,14 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import{library} from "@fortawesome/fontawesome-svg-core";
+import {faAnglesLeft, faArrowRightFromBracket, faBars,} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faAnglesLeft);
+library.add(faBars);
+library.add(faArrowRightFromBracket);
+
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -12,9 +20,10 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        return createApp({render: () => h(App, props)})
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .component('font-awesome-icon', FontAwesomeIcon)
             .mount(el);
     },
     progress: {
