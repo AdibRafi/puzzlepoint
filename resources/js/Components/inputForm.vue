3f25ee0d890ed79958c095c511753028fb92fa19
Abdjb <63862086+Abdjb@users.noreply.github.com>
2023-03-01 16:41:22 +0800

    moved reusable  related from jigsaw

---
+++ resources/js/Components/inputForm.vue
@@ -0,0 +1,31 @@
+<template>
+<!--    <label :for="labelFor" class="block font-medium text-sm text-gray-700">{{ labelName }}</label>-->
+<!--    <input :type="inputType" :id="inputId" name="" required="required" autofocus="autofocus"-->
+<!--           class="rounded-md shadow-sm border border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 block mt-1 w-full"-->
+<!--    :value="modelValue" @input='$emit("update:modelValue",$event.target.value)' />-->
+    <div class="form-control w-full max-w-xs">
+        <label :for="labelFor" class="label">
+            <span class="label-text block font-medium text-gray-700">{{ labelName }}</span>
+        </label>
+        <input :type="inputType" :id="inputId" :placeholder="inputPlaceholder"
+               class="input input-primary input-bordered input-sm w-full max-w-xs"
+        :value="modelValue" @input='$emit("update:modelValue",$event.target.value)'/>
+    </div>
+</template>
+
+<script setup>
+import {ref, defineProps} from 'vue'
+
+const props = defineProps({
+    labelName: String,
+    labelFor: String,
+    inputType: String,
+    inputId: String,
+    inputPlaceholder: String,
+    modelValue:String,
+});
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/submitButton.vue
@@ -0,0 +1,23 @@
+<template>
+<!--    <button @click="buttonMethod" type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800-->
+<!--                   border border-transparent rounded-md font-semibold text-xs text-white uppercase-->
+<!--                   tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none-->
+<!--                   focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25-->
+<!--                   transition ease-in-out duration-150 ml-3">{{ buttonName }}</button>-->
+
+    <button class="btn btn-primary"
+    @click="buttonMethod">{{ buttonName }}</button>
+</template>
+
+<script setup>
+import {defineProps} from 'vue';
+
+const props = defineProps({
+    buttonName: String,
+    buttonMethod: String
+})
+</script>
+
+<style scoped>
+
+</style>
