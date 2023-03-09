b3c538419b3ed4f369a5c6e9ea9a9abad3da5377
Abdjb <63862086+Abdjb@users.noreply.github.com>
2023-03-09 18:14:31 +0800

    update test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Assessment;
+use App\Models\Attendance;
 use App\Models\Classroom;
+use App\Models\Material;
+use App\Models\Module;
+use App\Models\Question;
+use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function index()
     {
-        $data = Classroom::all();
+//        $data = auth()->user()->attendances()->get();
+        $data = Assessment::find(2)->users()->get();
+
+//        $data = Assessment::find(1)->questions()->get();
+//        $data = Question::find(3)->assessment()->get();
+
         return inertia('Test',compact('data'));
     }
 }
--- resources/js/Pages/Test.vue
+++ resources/js/Pages/Test.vue
@@ -1,11 +1,33 @@
 <template>
+    <Head title="Test"/>
     <MainLayout>
-
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="card-body">
+                <h2 class="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
+        </div>
+        <div class="card w-96 bg-base-100 shadow-xl my-6">
+            <div class="card-body">
+                <h2 class="card-title">{{ data }}</h2>
+            </div>
+        </div>
+        <div v-for="data in data">
+            <div class="card w-96 bg-base-100 shadow-xl my-6">
+                <div class="card-body">
+                    <h2 class="card-title">{{ data.name }}</h2>
+                </div>
+            </div>
+        </div>
     </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from "@/Layouts/MainLayout.vue";
+import {Head} from "@inertiajs/vue3";
 
 defineProps({
     data: Object,
