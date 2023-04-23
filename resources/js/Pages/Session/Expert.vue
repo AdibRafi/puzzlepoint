d061748670098f382463db67296344e1b881eab1
Aqilah Ahmad Rafi <aqilah.rafi01@gmail.com>
2023-04-23 23:17:15 +0800

    added fully function timer

--- resources/js/Pages/Session/Expert.vue
+++ resources/js/Pages/Session/Expert.vue
@@ -1,18 +1,14 @@
 <template>
+    <Head title="Expert"/>
     <MainLayout>
-        <!--        <Card title="DEVELOPER">-->
-        <!--            <div v-for="items in props.jigsawGroupUserModal">-->
-        <!--                <p>{{ items.name }}</p>-->
-        <!--                <div v-for="items2 in items.users">-->
-        <!--                    {{items2.name}}-->
-        <!--                </div>-->
-        <!--            </div>-->
-        <!--        </Card>-->
+        <Card title="DEVELOPER">
+            <button class="btn btn-primary" @click="buttonTest">Reload</button>
+        </Card>
         <Card title="Expert Session" class="my-4">
             <p>{{ props.topicModuleModal.name }}</p>
         </Card>
         <Card title="Timer" class="my-4">
-
+            <p class="justify-center flex text-2xl">{{minuteCounter}}:{{secondCounter}}</p>
         </Card>
         <div v-for="groupData in props.expertGroupUserModal">
             <Card :title="groupData.name" class="my-4">
@@ -34,7 +30,8 @@
 <script setup>
 import MainLayout from "@/Layouts/MainLayout.vue";
 import Card from "@/Components/Card.vue";
-import {Link} from "@inertiajs/vue3";
+import {Head, Link, router} from "@inertiajs/vue3";
+import {ref} from "vue";
 
 const props = defineProps({
     topicModuleModal: Object,
@@ -43,6 +40,23 @@ const props = defineProps({
     expertGroupUserModal: Object,
 
 })
+
+const minuteCounter = ref(props.topicModuleModal.max_time_expert);
+const secondCounter = ref(0);
+
+const buttonTest = () => {
+    console.log('nice');
+    router.reload();
+}
+
+setInterval(() => {
+    if (secondCounter.value > 0) {
+        secondCounter.value--;
+    } else {
+        secondCounter.value = 59;
+        minuteCounter.value--;
+    }
+}, 1000)
 </script>
 
 <style scoped>
