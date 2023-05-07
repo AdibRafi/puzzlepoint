b531eb51554056ba81dc78c70efdfa424c7f9d15
Abdjb <63862086+Abdjb@users.noreply.github.com>
2023-05-07 13:14:12 +0800

    added event related into api routes

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,12 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MoveExpertSession;
+use App\Events\MoveJigsawSession;
+use App\Events\StudentAttendance;
+use App\Events\TimeSession;
+use App\Models\Topic;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EventController extends Controller
 {
-    public function studentAttendance()
+    public function studentAttendance($topic_id)
     {
+        $topicModal = Topic::find($topic_id);
 
+        $a = $topicModal->attendances()->get();
+        $b = Auth::user()->attendances()->get();
+        $c = collect();
+        $c = $c->merge($a)->merge($b)->duplicates()->values();
+
+        foreach ($c as $value) {
+            $value->update([
+                'attend_status' => 'present'
+            ]);
+        }
+
+        broadcast(new StudentAttendance(Auth::user()->name . ' is present'));
+    }
+
+    public function timeSession($minuteCounter, $secondCounter,
+                                $transitionMinuteCounter, $transitionSecondCounter)
+    {
+        TimeSession::dispatch($minuteCounter, $secondCounter, $transitionMinuteCounter, $transitionMinuteCounter);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Events\MoveExpertSession;
+use App\Events\MoveJigsawSession;
 use App\Http\Controllers\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +21,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('student-attendance',[EventController::class,'studentAttendance'])->name('event.student-attendance');
+Route::get('student-attendance', [EventController::class, 'studentAttendance'])->name('event.student-attendance');
+
+Route::get('move-expert-session', function () {
+    MoveExpertSession::dispatch();
+})->name('event.move-expert-session');
+
+Route::get('move-jigsaw-session', function () {
+    MoveJigsawSession::dispatch();
+})->name('event.move-jigsaw-session');
+
+Route::get('time-session', [EventController::class, 'timeSession'])->name('event.time-session');
