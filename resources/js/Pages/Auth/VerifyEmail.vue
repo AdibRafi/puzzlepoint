f03bddc33400559e07319ceef2aaa6e8106cd5b5
Abdjb <63862086+Abdjb@users.noreply.github.com>
2023-07-07 00:52:58 +0800

    update

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,32 +1,5 @@
 <template>
-    <!--    <GuestLayout>-->
     <Head title="Email Verification"/>
-
-    <!--        <div class="mb-4 text-sm text-gray-600">-->
-    <!--            Thanks for signing up! Before getting started, could you verify your email address by clicking on the link-->
-    <!--            we just emailed to you? If you didn't receive the email, we will gladly send you another.-->
-    <!--        </div>-->
-    <!---->
-    <!--        <div class="mb-4 font-medium text-sm text-green-600" v-if="verificationLinkSent">-->
-    <!--            A new verification link has been sent to the email address you provided during registration.-->
-    <!--        </div>-->
-    <!---->
-    <!--        <form @submit.prevent="submit">-->
-    <!--            <div class="mt-4 flex items-center justify-between">-->
-    <!--                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">-->
-    <!--                    Resend Verification Email-->
-    <!--                </PrimaryButton>-->
-    <!---->
-    <!--                <Link-->
-    <!--                    :href="route('logout')"-->
-    <!--                    method="post"-->
-    <!--                    as="button"-->
-    <!--                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"-->
-    <!--                    >Log Out</Link-->
-    <!--                >-->
-    <!--            </div>-->
-    <!--        </form>-->
-    <!--    </GuestLayout>-->
     <div class="min-h-screen bg-base-200 flex items-center">
         <div class="card mx-auto w-full max-w-5xl shadow-xl">
             <div class="grid md:grid-cols-2 grid-cols-1 bg-base-100 rounded-xl">
--- resources/js/Pages/Classroom/Index.vue
+++ resources/js/Pages/Classroom/Index.vue
@@ -1,38 +1,8 @@
 <template>
     <Head title="Dashboard"/>
     <Layout page-title="Classroom">
-<!--        <div v-if="wizardStatus !== 'onCreateClassroom'">-->
-<!--            <TitleCard title="Classroom" top-right-button-label="Add Class"-->
-<!--                       @button-function="router.get(route('classroom.create'))">-->
-<!--                <div v-if="wizardStatus === 'onCreateTopic'"-->
-<!--                     class="alert alert-info shadow-lg">-->
-<!--                    <div>-->
-<!--                        <font-awesome-icon icon="fa-solid fa-circle-info" size="lg" bounce/>-->
-<!--                        <span class="ml-2">Great! Click the Classroom that you created</span>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="grid mt-2 md:grid-cols-2 grid-cols-1 gap-6">-->
-<!--                    <div v-for="data in classroomData" :key="data">-->
-<!--                        <CardClick :title="data.name"-->
-<!--                                   @click="goClassroom(data.id)"-->
-<!--                                   :is_new="data.is_new">-->
-<!--                            <p>{{ data.subject_code }}</p>-->
-<!--                        </CardClick>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </TitleCard>-->
-<!--            &lt;!&ndash;            <TitleCard title="Classroom"&ndash;&gt;-->
-<!--            &lt;!&ndash;                       v-else-if="$page.props.auth.user.type === 'student'">&ndash;&gt;-->
-<!--            &lt;!&ndash;                <div v-for="data in classroomData" :key="data">&ndash;&gt;-->
-<!--            &lt;!&ndash;                    <CardClick :title="data.name" @click="goClassroom(data.id)"&ndash;&gt;-->
-<!--            &lt;!&ndash;                          class="cursor-pointer hover:bg-base-200">&ndash;&gt;-->
-<!--            &lt;!&ndash;                        <p>{{ data.subject_code }}</p>&ndash;&gt;-->
-<!--            &lt;!&ndash;                    </CardClick>&ndash;&gt;-->
-<!--            &lt;!&ndash;                </div>&ndash;&gt;-->
-<!--            &lt;!&ndash;            </TitleCard>&ndash;&gt;-->
-<!--        </div>-->
         <div v-if="!$page.props.auth.user.is_wizard_complete">
-            <TitleCard title="Classroom"
+            <TitleCard title="Classroom List"
                        :top-right-button-label="wizardStatus === 'onCreateClassroom' ? 'Add Class' : ''"
                        @button-function="router.get(route('classroom.create'))"
                        :tooltip-btn-text="wizardStatus === 'onCreateClassroom' ? 'Lets Start by adding a class' : ''">
@@ -76,7 +46,7 @@
             </div>
         </div>
         <div v-else>
-            <TitleCard title="Classroom" top-right-button-label="Add Class"
+            <TitleCard title="Classroom List" top-right-button-label="Add Class"
                        @button-function="router.get(route('classroom.create'))">
                 <div class="grid mt-2 md:grid-cols-2 grid-cols-1 gap-6">
                     <div v-for="data in classroomData" :key="data">
@@ -89,37 +59,6 @@
                 </div>
             </TitleCard>
         </div>
-
-        <!--        <div v-else-if="wizardStatus === 'onCreateClassroom'">-->
-        <!--            <div v-if="$page.props.auth.user.type === 'lecturer'">-->
-        <!--                <TitleCard title="Classroom" :top-right-button-label="wizardStatus === 'onCreateClassroom' ? 'Add Class' : ''"-->
-        <!--                           v-if="$page.props.auth.user.type === 'lecturer'"-->
-        <!--                           @button-function="router.get(route('classroom.create'))"-->
-        <!--                           tooltip-btn-text="Lets Start with Adding a Class">-->
-        <!--                </TitleCard>-->
-
-        <!--                <input type="checkbox" id="my-modal" class="modal-toggle"-->
-        <!--                       v-model="openModal"/>-->
-        <!--                <div class="modal">-->
-        <!--                    <div class="modal-box">-->
-        <!--                        <h3 class="font-bold text-lg">Welcome to PuzzlePoint-->
-        <!--                            <span class="mx-4">-->
-        <!--                        <font-awesome-icon icon="fa-solid fa-hand" shake size="xl"/>-->
-        <!--                     </span>-->
-        <!--                        </h3>-->
-        <!--                        <p class="py-4">Let's get started! Are you up for a tutorial?</p>-->
-        <!--                        <div class="modal-action">-->
-        <!--                            <button @click.prevent="updateWizard"-->
-        <!--                                    class="btn btn-warning">No-->
-        <!--                            </button>-->
-        <!--                            <button @click.prevent="openModalFunction"-->
-        <!--                                    class="btn btn-primary">Yes!-->
-        <!--                            </button>-->
-        <!--                        </div>-->
-        <!--                    </div>-->
-        <!--                </div>-->
-        <!--            </div>-->
-        <!--        </div>-->
     </Layout>
 
 </template>
@@ -127,8 +66,6 @@
 
 <script setup>
 import {Head, Link, router, usePage} from "@inertiajs/vue3";
-import MainLayout from "@/Layouts/MainLayout.vue";
-import Card from "@/Components/Card.vue";
 import Layout from "@/Layouts/Layout.vue";
 import TitleCard from "@/Components/TitleCard.vue";
 import {onMounted, ref} from "vue";
@@ -165,28 +102,4 @@ const destroy = (id) => {
     }
 }
 
-const datas2 = [
-    {
-        name: 'Class1',
-        code: 'TGD1202'
-    },
-    {
-        name: 'Class2',
-        code: 'TCP1302'
-    },
-    {
-        name: 'Class3',
-        code: 'TMU2231'
-    },
-    {
-        name: 'Class4',
-        code: 'TSP3301'
-    }
-
-]
-
 </script>
-
-<style scoped>
-
-</style>
